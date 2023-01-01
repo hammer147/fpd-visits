58b0affbfc3c3146f46ed51085be61c506e0ae1b
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2023-01-01 12:26:05 +0100

    head meta data and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,26 @@
 'use client'
 
-import type { NextPage } from 'next'
-// import Head from 'next/head'
 import styles from './page.module.css'
 import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react'
 import { useState } from 'react'
 
 const Home = () => {
   const [extendedResult, updateExtendedResult] = useState(false)
-  const { isLoading, error, data, getData } = useVisitorData({ extendedResult }, { immediate: true })
+  const { isLoading, error, data, getData } = useVisitorData(
+    { extendedResult },
+    { immediate: true }
+  )
 
   const reloadData = () => {
     getData({ ignoreCache: true })
   }
 
-  const onChangeExtendedResult = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const onChangeExtendedResult = (e: React.ChangeEvent<HTMLInputElement>) => {
     updateExtendedResult(e.target.checked)
   }
 
   return (
     <div className={styles.container}>
-      {/* <Head>
-        <title>FingerprintJS Pro NextJS Demo</title>
-        <meta name='description' content='Check if fingerprintjs-pro-react integration works with NextJS SSR' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head> */}
-
       <h1>FingerprintJS Pro NextJS Demo</h1>
       <div className={styles.testArea}>
         <div className={styles.controls}>
@@ -38,7 +33,8 @@ const Home = () => {
           </label>
         </div>
         <h4>
-          VisitorId: <span className={styles.visitorId}>{isLoading ? 'Loading...' : data?.visitorId}</span>
+          VisitorId:{' '}
+          <span className={styles.visitorId}>{isLoading ? 'Loading...' : data?.visitorId}</span>
         </h4>
         <h4>Full visitor data:</h4>
         <pre className={styles.data}>{error ? error.message : JSON.stringify(data, null, 2)}</pre>
